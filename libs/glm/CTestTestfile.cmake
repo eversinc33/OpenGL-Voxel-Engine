50dcbabbf19c34e8e1b621ccd539a16ba92eca9e
fumamatar <svrath@web.de>
2021-06-25 18:28:13 +0200

    remove libs

--- libs/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sven/Documents/libs/glm/glm
-# Build directory: /home/sven/Documents/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
